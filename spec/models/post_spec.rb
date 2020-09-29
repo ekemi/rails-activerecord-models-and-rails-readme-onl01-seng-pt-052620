550040d3c2e4cc6ddf2e9b67dc8aacac97729d1d
edmundo papi <edmundopierre@yahoo.com>
2020-09-29 15:04:32 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,7 @@
 require 'rails_helper'
 describe Post do
+  it 'can be create' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect (post).to be_valid
+  end
 end
